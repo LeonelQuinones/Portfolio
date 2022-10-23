b85f3c42e3407ca09b793a67ab7b909e1533a583
leoqdev <leonelquinones9@gmail.com>
2022-10-22 21:06:04 -0300

    seccion proyectos

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ traerLenguajes().then(info => {
         `
     })
 })
+
+let proyectos = document.getElementById('proyectos')
+
+traerProyectos().then(proy => {
+    proy.forEach((p) => {
+        proyectos.innerHTML += `
+            <div class="card col-sm-4 col-10 card-proyectos" style="width: 18rem;">
+                <img src="${p.img}" class="card-img-top" alt="${p.img}">
+                <div class="card-body">
+                    <h5 class="card-title">${p.nombre}</h5>
+                    <p class="card-text">${p.descripcion}</p>
+                    <a href="#">
+                        <i class="fa-brands fa-github"></i>
+                    </a>
+                    <a href="#">
+                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                    </a>
+                </div>
+            </div>
+        `
+    })
+})
