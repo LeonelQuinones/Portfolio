224b8ce8e540979c2f4eff6ef72092cbce98833f
leoqdev <leonelquinones9@gmail.com>
2022-11-15 22:20:01 -0300

    Estilos a las cards

--- script.js
+++ script.js
@@ -25,15 +25,19 @@ traerProyectos().then(proy => {
     proy.forEach((p) => {
         proyectos.innerHTML += `
             <div class="card col-sm-4 col-10 card-proyectos" style="width: 18rem;">
-                <img src="${p.img}" class="card-img-top" alt="${p.img}">
+                <a href="${p.urlpag}" class="div-img-card a-proyectos">
+                    <img src="${p.img}" class="card-img-top img-card" alt="${p.nombre}">
+                </a>
                 <div class="card-body">
-                    <h5 class="card-title">${p.nombre}</h5>
-                    <p class="card-text">${p.descripcion}</p>
-                    <a href="#">
-                        <i class="fa-brands fa-github"></i>
+                    <h4 class="titulo-card">${p.nombre}</h4>
+                    <p class="texto-card">${p.descripcion}</p>
+                    <h5 class="subtitulo-card">Herramientas:</h5>
+                    <p class="herramientas-card">${p.herramientas}</p>
+                    <a href="${p.urlgh}" class="a-proyectos">
+                        <i class="fa-brands fa-github iconos-proyectos"></i>
                     </a>
-                    <a href="#">
-                        <i class="fa-solid fa-arrow-up-right-from-square"></i>
+                    <a href="${p.urlpag}" class="a-proyectos">
+                        <i class="fa-solid fa-arrow-up-right-from-square iconos-proyectos"></i>
                     </a>
                 </div>
             </div>
